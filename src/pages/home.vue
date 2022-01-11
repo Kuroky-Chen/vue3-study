9849f37590d55c693d723b4f737c9b97f8d1fd07
Kuroky-Chen <5837907@qq.com>
2022-01-11 21:35:59 +0800

    no message

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,10 +2,12 @@
 <template>
     <h1>home</h1>
 	<TodoList />
+	<TodoListJsx />
     <!-- <Count /> -->
 </template>
 
 <script setup>
 	import TodoList from '../components/Todolist.vue'
+	import TodoListJsx from '../components/Todo.jsx'
   import Count from '../components/Count.vue'
 </script>
